fef0ec00bb82bc530c72d35f44777e4a3f41ba49
Geofrey Bruno <bruniquedevelopers@gmail.com>
2026-01-02 17:50:52 +0300

    few changes have been made

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -166,7 +166,7 @@ $Year=  date('Y');
 			<div class="second-footer-container p-5p">
 				<!---->
 				<p id="copyrightpara"><i>Copyright &copy; {{$Brandname}}  <?php echo $Year; ?> </i></p>
-				<div id="developerpara"><a href="mailto:bruniquedevelopers@gmail.com" class="color-black-dark">designed by bruniquedevelopers.com</div>
+				<div id="developerpara"><a href="https://bruniquedevelopers.com/" class="color-black-dark">designed by bruniquedevelopers.com</div>
 			</div>
 		</div> <!---end of second-footer---->
 
